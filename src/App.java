7f5c1e7e05c1e37c0173b446c17fbf6238552d55
LeonCzerwinski <poriusz@wp.pl>
2020-06-13 17:59:19 +0200

    added a short comment

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Math.PI -> "+ Math.PI);
 		System.out.println("Leibniz PI -> " + calculateSequenceFor(300));
+		/*
+		 * Pass the number of fraction pairs
+		 * for ex. -1/3 + 1/5 - 1/7 + 1/9 is considered as 2
+		 */
 	}
 	
 	public static double calculateSequenceFor(int n) {
